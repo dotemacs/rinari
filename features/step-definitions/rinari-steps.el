1c899cacf2033e5c9c5145420d4507dc249db298
Johan Andersson <johan.rejeep@gmail.com>
2010-04-05 14:52:54 +0000

    Added feature for setting TAGS file location.

---
+++ features/step-definitions/rinari-steps.el
@@ -0,0 +1,19 @@
+(Given "Rails application \"\\(.+\\)\""
+       (lambda (name)
+         (let ((app (expand-file-name name (expand-file-name "tmp" rinari-root-path))))
+           (setq current-rails-application app)
+           (shell-command (concat "rails _2.3.5_ -q " app)))))
+
+(Then "TAGS should be set to \"\\(.+\\)\""
+      (lambda (tags)
+        (should tags)
+        (should tags-file-name)
+        (should (string-match-p tags tags-file-name))))
+
+(When "I visit \"\\(.+\\)\""
+      (lambda (file)
+        (find-file (expand-file-name file current-rails-application))))
+
+(When "I create a TAGS file"
+      (lambda ()
+        (write-file (expand-file-name "TAGS" current-rails-application))))
--- features/support.el
+++ features/support.el
@@ -1,9 +1,24 @@
+(defvar current-rails-application nil
+  "Path to current active Rails application.")
+
 (let ((current-directory (file-name-directory load-file-name)))
   (setq rinari-root-path (expand-file-name ".." current-directory))
   (setq rinari-util-path (expand-file-name "util" rinari-root-path)))
 
 (add-to-list 'load-path rinari-root-path)
 (add-to-list 'load-path (expand-file-name "espuds" (expand-file-name "test" rinari-util-path)))
+(add-to-list 'load-path (expand-file-name "ert" (expand-file-name "test" rinari-util-path)))
 
 (require 'rinari)
 (require 'espuds)
+(require 'ert)
+
+(Before
+ (shell-command (concat "mkdir " (expand-file-name "tmp" rinari-root-path)))
+ )
+
+(After
+ (shell-command (concat "rm -Rf " (expand-file-name "tmp" rinari-root-path)))
+
+ (setq rinari-minor-mode-hook '())
+ )
