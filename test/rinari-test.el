13a2990affca979b21efd431a049f53c06610737
Johan Andersson <johan.rejeep@gmail.com>
2010-04-05 16:18:14 +0000

    Added test for default mode prefixes.

--- test/rinari-test.el
+++ test/rinari-test.el
@@ -36,6 +36,10 @@
   (should-match-partial "render :partial    =>    @partial" "partial")
   )
 
+(ert-deftest rinari-mode-prefixes ()
+  (should (equal 2 (length rinari-minor-mode-prefixes)))
+  (should (member ";" rinari-minor-mode-prefixes))
+  (should (member "'" rinari-minor-mode-prefixes)))
 (defun should-match-partial (line partial)
   "Asserts that LINE, which is a partial call matches
 `rinari-partial-regex' and that there's only one grouping, which is
