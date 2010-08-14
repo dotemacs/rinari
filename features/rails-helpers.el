713948b158ffeea48d6635c0cda5f20bb5ae5035
Johan Andersson <johan.rejeep@gmail.com>
2010-08-14 11:25:42 +0200

    Make sure stuff does not already exist.

--- features/rails-helpers.el
+++ features/rails-helpers.el
@@ -18,24 +18,37 @@
                   (kill-buffer-and-return-version buffer 3)))))))))
 
 (defun generate-app (name)
-  "Generates a Rails application with NAME."
+  "Generates a Rails application with NAME if it does not exist."
   (let* ((tmp-dir (expand-file-name "tmp" rinari-root-path))
          (app (expand-file-name name tmp-dir)))
-    (setq current-rails-application app)
-    (shell-command
-     (if (= (rails-version) 2)
-         (concat "rails " app " -q")
-       (concat "rails new " app " -q")))))
+    (unless (file-exists-p app)
+      (setq current-rails-application app)
+      (shell-command
+       (if (= (rails-version) 2)
+           (concat "rails " app " -q")
+         (concat "rails new " app " -q"))))))
 
 (defun generate-scaffold (name)
   "Generates a scaffold with name."
-  (let ((script-dir (expand-file-name "script" current-rails-application)))
-    (cond ((= (rails-version) 2)
-           (shell-command
-            (concat (expand-file-name "generate" script-dir) " scaffold " name)))
-          (t
-           (shell-command
-            (concat (expand-file-name "rails" script-dir) " generate scaffold " name " -q"))))))
+  (unless (scaffold-exists-p name)
+    (let ((script-dir (expand-file-name "script" current-rails-application)))
+      (cond ((= (rails-version) 2)
+             (shell-command
+              (concat (expand-file-name "generate" script-dir) " scaffold " name)))
+            (t
+             (shell-command
+              (concat (expand-file-name "rails" script-dir) " generate scaffold " name " -q")))))))
+
+(defun scaffold-exists-p (name)
+  "Checks if scaffold with NAME exists."
+  (file-exists-p
+   (expand-file-name
+    (concat (downcase name) ".rb")
+    (expand-file-name
+     "models"
+     (expand-file-name
+      "app"
+      current-rails-application)))))
 
 
 (provide 'rails-helpers)
